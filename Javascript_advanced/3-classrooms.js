95d04712f3137845370f3ad10b5ac250dc5ec679
WoodsonTD <taylordwoodson@gmail.com>
2023-07-06 22:44:11 -0500

    add

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,15 @@
+function createClassRoom(numberOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        };
+    }
+    let students = [];
+    for (var i = 0; i < numberOfStudents; i++) {
+        students.push(studentSeat(i + 1)());
+    }
+    return students;
+}
+
+var classRoom = createClassRoom(10);
+console.log(classRoom);
